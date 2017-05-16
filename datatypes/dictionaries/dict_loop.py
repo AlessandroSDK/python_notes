00023a6d030893b5cde068bd88ef4959d2890e2a
abresciani <abresciani@salesforce.com>
2017-05-16 11:40:16 +0100

    more on dictionaries

--- datatypes/dictionaries/dict_loop.py
+++ datatypes/dictionaries/dict_loop.py
@@ -1,3 +1,7 @@
+#Exercise 11.3. Dictionaries have a method called keys that returns the keys of the dictionary, in
+#no particular order, as a list.
+#Modify print_hist to print the keys and their values in alphabetical order.
+
 from dict_as_set_of_counters import histogram
 
 def print_hist(dct):
